a0bc3c523ae118752213922ffd83fb33ca03a4a4
Dhiraj Kumar <dhira5.kumar@gmail.com>
2025-05-23 21:50:00 +0530

    added jsp page

--- src/main/java/com/dhiraj/springboot/jenkins/controller/TestController.java
+++ src/main/java/com/dhiraj/springboot/jenkins/controller/TestController.java
@@ -5,11 +5,16 @@ import java.time.LocalDate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @RestController
 @RequestMapping("/test")
 public class TestController {
+	@GetMapping("/home")
+	public ModelAndView home() {
+		return new ModelAndView("index") ;
+	}
 	@GetMapping("/welcome")
 	public String welcomeMsg() {
 		LocalDate str=LocalDate.now();
